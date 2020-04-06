97e2f7ba2fce29f1af6c26b17092c8f94a00668a
Abai Alihan <alihan.abai@yandex.com>
2020-04-06 20:26:11 +0600

    make rest api with get post put delete

---
+++ insta2/src/main/java/com/abai/insta2/Insta2Application.java
@@ -0,0 +1,13 @@
+package com.abai.insta2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Insta2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Insta2Application.class, args);
+	}
+
+}
---
+++ insta2/src/test/java/com/abai/insta2/Insta2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abai.insta2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Insta2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
