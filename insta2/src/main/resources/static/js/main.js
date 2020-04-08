b289abb295165d09a0e6bbc220289007611e215f
Abai Alihan <alihan.abai@yandex.com>
2020-04-09 03:42:17 +0600

    continue connected Vue.js -2

--- insta2/src/main/resources/static/js/main.js
+++ insta2/src/main/resources/static/js/main.js
@@ -1,18 +1,88 @@
 
+function getIndex(list, id){
+    for (var i = 0; i < list.length; i++){
+        if (list[i].id === id){
+            return i;
+        }
+    }
+    return -1;
+}
+
 var messageApi = Vue.resource('/message{/id}');
+
+Vue.component('message-form', {
+  props: ['messages', 'messageAttr'],
+  data: function() {
+        return {
+            text: '',
+            id: ''
+        }
+  },
+  watch: {
+        messageAttr: function(newVal, oldVal){
+            this.text = newVal.text;
+            this.id = newVal.id;
+        }
+  },
+  template:
+    '<div>' +
+        '<input type="text" placeholder="Input text" v-model="text" />' +
+        '<input type="button" value="Save" @click="save" />' +
+    '</div>',
+    methods: {
+        save: function() {
+            var message = { text: this.text };
+
+            if (this.id){
+                messageApi.update({id: this.id}, message).then(result =>
+                    result.json().then(data => {
+                        var index = getIndex(this.messages, data.id);
+                        this.messages.splice(index, 1, data);
+                        this.text = ''
+                        this.id = ''
+                    })
+                )
+            } else {
+                messageApi.save({}, message).then(result =>
+                    result.json().then(data => {
+                      this.messages.push(data);
+                      this.text = ''
+                    })
+                )
+            }
+        }
+    }
+});
 
 // Определяем новый компонент под именем message-row
 Vue.component('message-row', {
-  props: ['message'],
-  template: '<div><i> ({{message.id}}) </i> {{ message.text }} </div>'
+  props: ['message', 'editMethod'],
+  template:
+    '<div>' +
+        '<i> ({{message.id}}) </i> {{ message.text }}' +
+        '<span>' +
+            '<input type="button" value="Edit" @click="edit" />' +
+        '</span>' +
+     '</div>',
+     methods: {
+        edit: function() {
+            this.editMethod(this.message);
+        }
+     }
 });
 
 // Определяем новый компонент под именем messages-list
 Vue.component('messages-list', {
   props: ['messages'],
+  data: function() {
+    return {
+        message: null
+    }
+  },
   template:
     '<div>' +
-        '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+        '<message-form :messages="messages" :messageAttr="message" />' +
+        '<message-row v-for="message in messages" :key="message.id" :message="message" :editMethod="editMethod" />' +
     '</div>',
    created: function(){
      messageApi.get().then(result =>
@@ -20,6 +90,11 @@ Vue.component('messages-list', {
             data.forEach(message => this.messages.push(message))
         )
      )
+   },
+   methods: {
+        editMethod: function(message) {
+            this.message = message;
+        }
    }
 });
 
